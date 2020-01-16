6a63adefc6424e9f51b19b675648c120f5f15297
hahohe1892 <t.frank95@web.de>
2020-01-16 10:32:05 +0100

    !! not fixed front even though I thought so!! , but with von mises calving. GeomProj_extenddomain_SG_spcvx300_NL-500_FrM200_FlMreal30_FC35_FT25_TS0.01_OF100_hmin350_BuH0_BuP0_BuS0_ByH0_ByP0_ByS0_Stallo_IF90000_MS1000000_newMassSpc_MSF100000_xdim92000_finestMesh_noMinCalv_accT50_parStart10_inH0_fixedFrontwithCalving; 2037073 OUTCOME: massive retreat to 40 km, would have gone much further.

--- my_Runner2.py
+++ my_Runner2.py
@@ -144,7 +144,7 @@ def SpinUp_load(params, run_name, load_name):
     return md
 
 def extenddomain(params, run_name, load_name):
-    restart_time=50
+    restart_time=25
     y_dim, x_dim, slope, dc, gap_halfwidth, step = standardvalues()
     start_icefront=params['start_icefront']
     slab_thickness=params['slab_thickness']
@@ -164,7 +164,7 @@ def extenddomain(params, run_name, load_name):
     old_mesh_geometry=md.geometry.bed
     h=np.nan*np.ones(md.mesh.numberofvertices)
     #h[np.where(np.logical_and(np.logical_and(md.mesh.y<17800, md.mesh.y>12200), np.logical_and(md.mesh.x<65000, md.mesh.x>20000)))]=100  #25000
-    h[np.where(np.logical_and(md.geometry.bed<1200, np.logical_and(md.mesh.x<70000, md.mesh.x>40000)))]=100
+    h[np.where(np.logical_and(md.geometry.bed<1200, np.logical_and(md.mesh.x<92000, md.mesh.x>60000)))]=100
     
     md=bamg(md, 'field', old_mesh_geometry, 'hmax', 1000, 'hmin', params['hmin'], 'gradation', 1.7, 'hVertices', h)
     md.miscellaneous.name=run_name
