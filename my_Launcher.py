dd8d4ba431156d8a5e04656b74408a65dc3d8b25
hahohe1892 <t.frank95@web.de>
2020-03-10 10:19:56 +0100

    retreat 240 bump with FrM1200 and FlM180. GeomProj_extenddomain_SG_spcvx50_NL-450_FrM1200_FlMreal180_FC40_FT300_TS0.01_OF100_hmin350_BuH240_BuP55000_BuS20000_ByH0_ByP0_ByS0_Stallo_IF60000_MS1000000_MSF200000_xdim84000_accT1225_inH0_smb55_smbPos30000_funnel300_FullMelt_noCutoff; 2154568 OUTCOME: retreats onto bump.

--- my_Launcher.py
+++ my_Launcher.py
@@ -41,7 +41,7 @@ y_dim, x_dim, slope, dc, gap_halfwidth, step = standardvalues()
 
 fixfront=False
 
-bump_height=[-240]
+bump_height=[240]
 bump_spread=[20000]
 bump_pos=[55000]
 bump_skew=[0]
@@ -66,8 +66,8 @@ min_thickness_mask=[1]
 spcvx=[50]
 hmin=[350]
 null_level=[-450]
-frontal_melt=[1000]
-floating_melt=[150]
+frontal_melt=[1200]
+floating_melt=[180]
 friction=[40]
 start_icefront=[60000]
 max_stress=[1000000]
@@ -80,7 +80,7 @@ final_time=[300]
 timestepping=[0.01]
 output_frequency=[100]
 
-x_dim=[85000]
+x_dim=[84000]
 ## define parameters lists
 
 bump_height_list=[]
@@ -294,7 +294,7 @@ which_run={'SpinUp':['SpinUp', 'SpinUp', 'dummy', 'Transient'],
            #'extenddomain':['extenddomain', 'extenddomain','GeomProj_SpinUp_load_SG_spcvx50_NL-450_FrM200_FlMreal30_FC40_FT100_TS0.01_OF100_hmin350_BuH0_BuP0_BuS0_ByH0_ByP0_ByS0_Stallo_IF60000_MS1000000_MSF200000_xdim85000_accT875_inH0_smb55_smbPos30000_funnel300_FullMelt.nc','Transient'],
           # 'extenddomain':['extenddomain', 'extenddomain','GeomProj_extenddomain_SG_spcvx50_NL-450_FrM200_FlMreal30_FC40_FT50_TS0.01_OF100_hmin350_BuH-120_BuP55000_BuS20000_ByH0_ByP0_ByS0_Stallo_IF60000_MS1000000_MSF200000_xdim85000_accT1125_inH0_smb55_smbPos30000_funnel300_FullMelt_noCutoff.nc','Transient'],
           # 'extenddomain':['extenddomain', 'extenddomain','GeomProj_extenddomain_SG_spcvx50_NL-450_FrM1200_FlMreal180_FC40_FT200_TS0.01_OF100_hmin350_BuH-120_BuP55000_BuS20000_ByH0_ByP0_ByS0_Stallo_IF60000_MS1000000_MSF200000_xdim85000_accT1125_inH0_smb55_smbPos30000_funnel300_FullMelt_noCutoff.nc','Transient'],
-           'extenddomain':['extenddomain', 'extenddomain','GeomProj_extenddomain_SG_spcvx50_NL-450_FrM200_FlMreal30_FC40_FT50_TS0.01_OF100_hmin350_BuH-240_BuP55000_BuS20000_ByH0_ByP0_ByS0_Stallo_IF60000_MS1000000_MSF200000_xdim85000_accT925_inH0_smb55_smbPos30000_funnel300_FullMelt_noCutoff.nc','Transient'],
+           'extenddomain':['extenddomain', 'extenddomain','GeomProj_extenddomain_SG_spcvx50_NL-450_FrM200_FlMreal30_FC40_FT50_TS0.01_OF100_hmin350_BuH240_BuP55000_BuS20000_ByH0_ByP0_ByS0_Stallo_IF60000_MS1000000_MSF200000_xdim85000_accT925_inH0_smb55_smbPos30000_funnel300_FullMelt_noCutoff.nc','Transient'],
            'insertatinflux':['insertatinflux', 'insertatinflux', 'GeomProj_extenddomain_SG_spcvx800_NL-500_FrM200_FlMreal30_FC40_FT50_TS0.01_OF100_hmin350_BuH0_BuP0_BuS0_ByH0_ByP0_ByS0_Stallo_IF60000_MS1000000_newMassSpc_MSF200000_xdim65000_finestMesh_noMinCalv_accT175_parStart10_inH0_fixedfrontandnocal.nc','Transient']
            }
 
