435840216c4f0c92f61cf32f46c8a6b84b216926
hahohe1892 <t.frank95@web.de>
2020-03-10 19:19:07 +0100

    retreat 900 embayment with FrM1200 and FlM180. GeomProj_extenddomain_SG_spcvx50_NL-450_FrM1200_FlMreal180_FC40_FT200_TS0.01_OF100_hmin350_BuH0_BuP0_BuS0_ByH900_ByP55000_ByS20000_Stallo_IF60000_MS1000000_MSF200000_xdim85000_accT1225_inH0_smb55_smbPos30000_funnel300_FullMelt_noCutoff; 2157538 OUTCOME: has retreated through embayment.

--- my_Launcher.py
+++ my_Launcher.py
@@ -19,7 +19,7 @@ clustername = 'Stallo'
 if clustername == 'Stallo':
     clusterident='Stallo'
     #cluster = stallo('numnodes', 1, 'cpuspernode', 16, 'time', 4 * 60, 'queue', 'devel')  # time in minutes
-    cluster = stallo('numnodes', 1, 'cpuspernode', 16, 'mem', 1.9, 'time', 30 * 60)  #time in minutes
+    cluster = stallo('numnodes', 1, 'cpuspernode', 16, 'mem', 1.9, 'time', 24 * 60)  #time in minutes
     #cluster = stallo('numnodes', 1, 'cpuspernode', 16, 'mem', 1.9, 'time', 10 * 60)  #time in minutes
 else:
     #cluster = generic('name', clustername, 'np', 1, 'valgrind', '/usr/bin/valgrind', 'valgrindlib', '/usr/lib/valgrind/libmpiwrap-amd64-linux.so')
@@ -66,8 +66,8 @@ min_thickness_mask=[1]
 spcvx=[50]
 hmin=[350]
 null_level=[-450]
-frontal_melt=[1000]
-floating_melt=[150]
+frontal_melt=[1200]
+floating_melt=[180]
 friction=[40]
 start_icefront=[60000]
 max_stress=[1000000]
@@ -76,7 +76,7 @@ influx_height=[0]
 
 
 
-final_time=[300]
+final_time=[200]
 timestepping=[0.01]
 output_frequency=[100]
 
